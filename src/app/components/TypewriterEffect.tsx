e65be9b5da056704b289b762f0a4ab4aefad3338
cgtyeo <cgtyeo@gmail.com>
2023-12-07 01:10:35 +0300

    done with bugs

--- src/app/components/TypewriterEffect.tsx
+++ src/app/components/TypewriterEffect.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
+import { text } from 'stream/consumers';
 
-const TypewriterEffect = () => {
+type TypewriterEffectProps = {
+    sampleText: string;
+    start: boolean
+}
+
+const TypewriterEffect: React.FC<TypewriterEffectProps> = ({ sampleText, start, displayedText,setDisplayedText }) => {
+   const [currentIndex,setCurrentIndex] = useState<number>(0)
+
+        useEffect(() => {
+            let interval: NodeJS.Timeout | number;
+            
+            if (sampleText && sampleText.length > 0 && currentIndex< sampleText.length && start == true) {
+             
+                interval = setInterval(() => {
+                    setDisplayedText((prev) => prev + sampleText[currentIndex]);
+                    setCurrentIndex((prevIndex) => prevIndex + 1)
+                 }, 10)
+                  
+                    return () => clearInterval(interval);
+                }
+},[currentIndex,typingDelay,start])
+
   return (
-    <div>
-      
+      <div>
+      <p>{displayedText}</p>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import TypewriterEffect from "./components/TypewriterEffect"
+import {useState} from "react"
+
+type TypewriterEffectProps = {
+  sampleText?: string;
+  typingDelay?: number;
+}
 
 export default function Home() {
+  const [start, setStart] = useState<boolean>(false);
+  const [displayedText, setDisplayedText] = useState<string>("");
+  const sampleTest:string = "Note that, the boolean Boolean is different from the lower case boolean type. The upper case Boolean is an object type whereas lower case boolean is a primitive type. It is recommended to use the primitive type boolean in your code, because, while JavaScript coerces an object to its primitive type, the TypeScript type system does not. TypeScript treats it like an object type."
+ 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("click",start)
+  setStart(!start)
+  }
+  
+  const handleClickReset = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setDisplayedText("")
+    setStart(false)
+  }
   return (
-    <main className={styles.main}>
-    hello
-    </main>
+    <div>
+      <div>
+        <button onClick={handleClick}>{start ? `Stop Generating` : `Start Generating`}</button>
+        <button onClick={handleClickReset}>Reset</button>
+      </div>
+      <TypewriterEffect sampleText={sampleTest} typingDelay={100} start={start} displayedText={displayedText} setDisplayedText={setDisplayedText} />
+
+    </div>
   )
 }
